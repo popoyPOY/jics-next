ce4c118a79f6db1d5c07812cd9f20d0e8264c515
Jaynel Patiarba <lenyajpatiarba@gmail.com>
2024-07-18 11:07:25 +0800

    adding production ulr

--- src/utils/pb.ts
+++ src/utils/pb.ts
@@ -3,10 +3,10 @@ import PocketBase from 'pocketbase';
 const timestamp = new Date().getTime();
 
 export const exp = timestamp + (60 * 60 * 24 * 1000 * 1);
-export const PB_URL = "http://127.0.0.1:8090";
+export const PB_URL = "https://pocketbase-zc4408o.91.205.104.219.sslip.io";
 
 export const phoneRegex = new RegExp(
   /^([+]?[\s0-9]+)?(\d{3}|[(]?[0-9]+[)])?([-]?[\s]?[0-9])+$/
 );
 
-export const pb = new PocketBase("http://127.0.0.1:8090");
+export const pb = new PocketBase(PB_URL);
